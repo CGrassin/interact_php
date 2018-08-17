0c46e23b3cd0392f59b044e95e405ebe3eebf817
CGrassin <42406777+CGrassin@users.noreply.github.com>
2018-08-17 15:01:55 +0200

    Initial commit

---
+++ lib/Interact_PHP/Interact_PHP.php
@@ -0,0 +1,160 @@
+<?php
+// TODO
+// * File with all texts
+// * better sanitation
+// * admin
+
+// Features :
+// * comments in xml
+// * Super simple setup (no DB, no login, no cookies, 2 lines to add a comment section to any page)
+// * Customizable style, easy to match look & feel to your website
+// * email on new comment
+// * PHP 5 & 7
+namespace Interact_PHP;
+
+require_once('settings.php');
+
+
+/* Entry function to display the comment zone. 
+Optionnal argument: Page title is the title of the current 
+article. You may want to set it juste to keep the folder clear;
+It will be taken from the url otherwise. */
+function Interact_PHP($pageTitle=NULL){
+
+  // If no title is provided, take it from the url (reoving the query string first)
+  if (is_null($pageTitle)) {
+    $pageTitle=strtok($_SERVER["REQUEST_URI"],'?');
+  }
+  $pageTitle=SanitizeFilename($pageTitle);
+  ?>
+
+  <link rel="stylesheet" type="text/css" href="<?php echo Settings::LIBRARY_ROOT.'/css/'.Settings::THEME_STYLESHEET; ?>">
+  <script src="<?php echo Settings::LIBRARY_ROOT.'/js/interact.min.js'; ?>"></script>
+
+  <div style="width: 100%">
+    <div class="comment-box">
+
+      <p class="comment-title"><?php echo Settings::TITLE_COMMENT_BOX; ?></p>
+
+      <?php
+      if (Settings::DISABLE_COMMENTS) {
+       echo '<p class="text-muted text-center">'.Settings::DISABLE_COMMENTS_MESSAGE.'</p>';
+     }
+     else { ?>
+
+       <noscript>
+        <style>#commentForm {display:none;}</style>
+        <p class="text-muted text-center">Please enable javascript to comment.</p>
+      </noscript>
+
+      <form class="comment-form" method="post" action="<?php echo Settings::LIBRARY_ROOT.'/postComment'; ?>" id="commentForm" onsubmit="return interactphpSubmit()">
+        <div id="interactphp-alert" class="hidden" role="alert">Error sending form...</div>
+
+        <label class="sr-only" for="interactphp-message">Comment</label>
+        <textarea id="interactphp-message" class="input" name="message" rows="3" required maxlength="1000" placeholder="Enter your comment..." onblur="recaptchaDisplay()"></textarea>
+
+        <div class="input-group">
+          <div class="interactphp-nickname">
+            <label class="sr-only" for="interactphp-name">Nickname</label>
+            <input id="interactphp-name" class="input" type="text" name="name" placeholder="Nickname" maxlength="30" required onblur="recaptchaDisplay()">
+          </div>
+
+          <div class="interactphp-submit">
+            <button class="input" type="submit"><span class="fa fa-comment"></span> Submit</button>
+          </div>
+        </div>
+
+        <input type="text" class="hidden" name="page" value="<?php echo $pageTitle?>">
+
+        <?php
+        if (!is_null(Settings::RECAPTCHA_PUBLIC_KEY)&&!is_null(Settings::RECAPTCHA_SECRET_KEY)) {
+          echo '<div id="google-recaptcha" class="text-center"><div style="display: inline-block;" class="g-recaptcha" data-sitekey="'.Settings::RECAPTCHA_PUBLIC_KEY.'"></div></div>';
+          echo "<script src='https://www.google.com/recaptcha/api.js'></script>";
+        } ?>
+
+      </form>
+
+    <?php } ?>
+
+    <ul class="comment-list list-unstyled">
+      <?php DisplayComments($pageTitle); ?>
+    </ul>
+  </div>
+</div>
+
+<?php
+}
+
+/* Adds a comment to the XML datafile $filename.
+SECURITY: In case the user hacked the form to enter a random/nasty 
+'$page', it will simply add a safely sanitized random file in the 
+'Commnents' directory, preventing any harm to be done even if the 
+whole filesystem is in 777.
+SECURITY 2: htmlspecialchars is used to prevent breaking the XML structure
+with nasty input. */
+function AddComment($page,$name,$message) {
+  try
+  {
+    $filename = NameToCommentFile($page);
+
+    if (!file_exists(Settings::COMMENTS_ROOT)) {
+      mkdir(Settings::COMMENTS_ROOT, 0777, true);
+    }
+
+    if (file_exists($filename)) 
+    {
+     $xml = simplexml_load_string(file_get_contents($filename));
+   } 
+   else 
+   {
+    $xml = simplexml_load_string('<?xml version="1.0" encoding="UTF-8"?><comments></comments>');
+  }
+
+  $commentElement = $xml->addChild('comment');
+  $commentElement->addChild('date', time());
+  $commentElement->addChild('name', htmlspecialchars($name));
+  $commentElement->addChild('message', htmlspecialchars($message));
+  if (Settings::ENABLE_SAVE_COMMENTER_IP) {
+    $commentElement->addChild('ip', $_SERVER['REMOTE_ADDR']);
+  }
+  $xml->asXML($filename);
+  return true;
+} catch(Exception $e){}
+return false;
+}
+
+/* Displays the comments for $filename, if any exists. Displays
+a cutomizable 'no comments' message otherwise.
+SECURITY: if for some reason, $page where to be compromized, the
+attacker would NOT be able to see anything else than comments thank
+to the restriction of 'NameToCommentFile'.*/
+function DisplayComments($page) {
+  $filename = NameToCommentFile($page);
+  if (file_exists($filename)) {
+    $xml= simplexml_load_string(file_get_contents($filename));
+    $count=0;
+    foreach ($xml->children() as $comment) {
+     $count++;
+     echo '<li class="comment">';
+     echo '<p class="comment-author"><span class="comment-rank">#'.$count.'</span> '.htmlspecialchars($comment->{"name"}).'</p>';
+     echo '<p class="comment-message">'.htmlspecialchars($comment->{"message"}).'</p>';
+     echo '<p class="comment-date text-muted">on '.date("F j Y, G:i", intval($comment->{"date"})).'</p>';
+     echo '</li>';
+   }
+ }
+ else {
+  echo '<li class="comment"><p>'.Settings::NO_COMMENTS_MESSAGE.'</p></li>';
+}
+}
+
+/* Sanitizes string to be used as a filename. It takes no chances, and
+works on any filesystem. */
+function SanitizeFilename($page) {
+  return substr(preg_replace( '/[^a-z0-9]+/', '-', strtolower( $page ) ),0,30);
+}
+
+/* Converts a page title to a safe filepath to save the comments. */
+function NameToCommentFile($page) {
+  return Settings::COMMENTS_ROOT.'/'.SanitizeFilename($page).'.xml';
+}
+?>
---
+++ lib/Interact_PHP/postComment.php
@@ -0,0 +1,79 @@
+<?php
+namespace Interact_PHP;
+
+require_once('settings.php');
+require_once('Interact_PHP.php');
+
+/* If comments are disabled but user tried to send a comment with 
+some manual POST request, forbid action. */
+if (Settings::DISABLE_COMMENTS) {
+  echo Settings::DISABLE_COMMENTS_MESSAGE;
+  exit;
+}
+
+/* If Google's reCAPTCHA are enabled, check the validity of the 
+CAPTCHA.*/
+if (!is_null(Settings::RECAPTCHA_PUBLIC_KEY)&&!is_null(Settings::RECAPTCHA_SECRET_KEY)) {
+  if (!isset($_POST['g-recaptcha-response'])) 
+  {
+    echo "Please enable Javascript and/or check the reCAPTCHA!";
+    exit;
+}
+else
+{
+    $api_url = "https://www.google.com/recaptcha/api/siteverify?secret=".Settings::RECAPTCHA_SECRET_KEY."&response=".$_POST['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR'];
+
+    $decode = json_decode(file_get_contents($api_url), true);
+
+    if ($decode['success'] == false) {
+      echo 'Please check the reCAPTCHA!';
+      exit;
+  }
+}
+}
+
+/* Check the presence of the POST fields. */
+if(!isset($_POST['name']) || !isset($_POST['message']) || !isset($_POST['page'])){
+    echo "System error... please try again later.";
+    exit;
+}
+
+/* Check the validity of the input. */
+$name=preg_replace("/\r|\n/", " ", $_POST['name']);
+if(strlen($name)<=0 || strlen($name)>30){
+    echo "Error: name must contain between 1 and 30 characters.";
+    exit;
+}
+$message=preg_replace("/\r|\n/", " ", $_POST['message']);
+if(strlen($message)<=0 || strlen($message)>1000){
+    echo "Error: your comment must contain between 1 and 1000 characters.";
+    exit;
+}
+$page=SanitizeFilename($_POST['page']);
+if(strlen($page)<=0){
+    echo "System error... please try again later.";
+    exit;
+}
+
+/* At this point, the input is valid. Add the comment to the XML file. */
+ignore_user_abort(true);
+set_time_limit(0);
+ob_start();
+
+if(AddComment($page,$name,$message)){
+    echo "ok";
+}else{
+    echo "System error... please try again later.";
+}
+
+/* Flush PHP buffer to answer to the AJAX call without having to wait for the
+completion of the callback function (which, in the case of emails is 2s+)*/
+header('Connection: close');
+header('Content-Length: '.ob_get_length());
+ob_end_flush();
+ob_flush();
+flush();
+
+/* Callback */
+Settings::CommentCallback($page,$name,$message);
+?>
